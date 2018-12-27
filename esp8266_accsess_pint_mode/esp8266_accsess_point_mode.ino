ed9ec74b50c2e0338d5a9c83930c807424d653a7
JanisDavidsons <janis.davidsons@gmail.com>
2018-12-27 18:16:34 +0200

    New functions - toggle button added.

--- esp8266_accsess_pint_mode/esp8266_accsess_point_mode.ino
+++ esp8266_accsess_pint_mode/esp8266_accsess_point_mode.ino
@@ -5,8 +5,11 @@
 
 ESP8266WebServer server (80);		// Create a webserver object that listens for HTTP request on port 80
 
+const int led = 2;
+
 void handleRoot();					// function prototypes for HTTP handlers
 void handleNotFound();				// function prototypes for HTTP handlers
+void handleLED();
 
 
 void setup()
@@ -18,22 +21,32 @@ void setup()
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
 
-  server.on("/", handleRoot);				// Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/",HTTP_GET, handleRoot);				// Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/LED", HTTP_POST, handleLED);			// Call the 'handleLED' function when a POST request is made to URI "/LED"
   server.onNotFound(handleNotFound);
-  server.begin();							// Actually start the server
+  server.begin();									// Actually start the server
   Serial.println("HTML server started!");
-
 }
 
 void loop() {
-	server.handleClient();					// Listen for HTTP requests from clients
-
+	server.handleClient();							// Listen for HTTP requests from clients
 }
 
 void handleRoot(){
-	server.send(200, "text/plain", "Hello World");		// Send HTTP status 200 (Ok) and send some text to the browser/client
+	server.send(200, "text/html", "<form action=\"/LED\" method=\"POST\"><input type=\"submit\" value=\"Toggle LED\"></form>");		// When URI / is requested, send a web page with a button to toggle the LED
+}
+
+void handleLED(){									// If a POST request is made to URI /LED
+	digitalWrite(led, !digitalRead(led));		 	// Change the state of the LED
+	server.sendHeader("Location", "/");				// Add a header to respond with a new location for the browser to go to the home page again
+	server.send(303);								// Send it back to the browser with an HTTP status 303 (See Other) to redirect
+	Serial.println("LED is now: ");
+	Serial.print("\t");
+	Serial.print(digitalRead(led));
 }
 
 void handleNotFound(){
 	server.send(404, "text/plain", "404: Not found");	// Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
+
+
