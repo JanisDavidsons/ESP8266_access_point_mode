4e0aeb0feb161883d9c0f475def6bbeb92d1ae7a
JanisDavidsons <janis.davidsons@gmail.com>
2018-12-27 14:13:36 +0200

    New code for client requests.

--- esp8266_accsess_pint_mode/esp8266_accsess_point_mode.ino
+++ esp8266_accsess_pint_mode/esp8266_accsess_point_mode.ino
@@ -1,26 +1,39 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WiFi.h>       		 // Include the Wi-Fi library
 #include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
 
-//
-//IPAddress local_IP(192,168,4,22);
-//IPAddress gateway(192,168,4,9);
-//IPAddress subnet(255,255,255,0);
+ESP8266WebServer server (80);		// Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();					// function prototypes for HTTP handlers
+void handleNotFound();				// function prototypes for HTTP handlers
+
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-
-//  Serial.print("Setting soft-AP configuration ... ");
-//  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-
   Serial.print("Setting soft-AP ... ");
   Serial.println(WiFi.softAP("ESP8266", "asdfqwerty") ? "Ready" : "Failed!");
-
   Serial.print("Soft-AP IP address = ");
   Serial.println(WiFi.softAPIP());
+
+  server.on("/", handleRoot);				// Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);
+  server.begin();							// Actually start the server
+  Serial.println("HTML server started!");
+
 }
 
-void loop() {}
+void loop() {
+	server.handleClient();					// Listen for HTTP requests from clients
+
+}
+
+void handleRoot(){
+	server.send(200, "text/plain", "Hello World");		// Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+	server.send(404, "text/plain", "404: Not found");	// Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
